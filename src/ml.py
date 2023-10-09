be3d40b54b50379aaa35894e7d5cd716b3557cd3
Andrea Palladino <andreapalladino@MacBook-Pro-di-Andrea.local>
2023-10-09 11:08:38 +0200

    use pylint on ml script

--- src/ml.py
+++ src/ml.py
@@ -4,18 +4,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-def train_evaluate_model(X_train, y_train, X_test, y_test, model):
+def train_evaluate_model(x_train, y_train, x_test, y_test, model):
 
     # scale data
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
 
     # fit model
-    print("Fitting the model with", X_train.shape[1], "features")
-    model.fit(X_train, y_train)
-    train_score = round(model.score(X_train, y_train), 4)
-    test_score = round(model.score(X_test, y_test), 4)
+    print("Fitting the model with", x_train.shape[1], "features")
+    model.fit(x_train, y_train)
+    train_score = round(model.score(x_train, y_train), 4)
+    test_score = round(model.score(x_test, y_test), 4)
     print("Train score", train_score)
     print("Test score", test_score)
 
@@ -40,7 +40,7 @@ def get_feature_importances(trained_model, column_names):
     return df_coef
 
 
-def select_relevant_features(df_coef, X, verbose):
+def select_relevant_features(df_coef, features, verbose):
 
     # select relevant features
     index_threshold = np.array(
@@ -50,42 +50,42 @@ def select_relevant_features(df_coef, X, verbose):
     relevant_features = relevant_features["Feature name"]
 
     if verbose:
-        print("Selected", len(relevant_features), "features out of", X.shape[1]-1)
+        print("Selected", len(relevant_features), "features out of", features.shape[1] - 1)
 
     # return simplified dataset, containing only relevant features
-    simplified_dataset = X.loc[:, relevant_features]
+    simplified_dataset = features.loc[:, relevant_features]
 
     return simplified_dataset
 
 
-def scan_features(X, y, model, verbose):
+def scan_features(features, labels, model, verbose):
 
     #  create train and test data
-    X_new = X.copy(deep=True)
-    X_new["random_feature"] = np.random.normal(0, 1, size=len(X_new))
-    X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=0.2)
+    x_new = features.copy(deep=True)
+    x_new["random_feature"] = np.random.normal(0, 1, size=len(x_new))
+    x_train, x_test, y_train, y_test = train_test_split(x_new, labels, test_size=0.2)
 
-    trained_model = train_evaluate_model(X_train, y_train, X_test, y_test, model)
+    trained_model = train_evaluate_model(x_train, y_train, x_test, y_test, model)
 
-    df_coef = get_feature_importances(trained_model, X_train.columns)
+    df_coef = get_feature_importances(trained_model, x_train.columns)
 
-    simplified_dataset = select_relevant_features(df_coef, X_new, verbose)
+    simplified_dataset = select_relevant_features(df_coef, x_new, verbose)
 
     return simplified_dataset
 
 
 def get_relevant_features(
-    X, y, model, epochs, patience, verbose=True, filename_output=False
+    features, labels, model, epochs, patience, verbose=True, filename_output=False
 ):
-    X_new = X.copy(deep=True)
+    x_new = features.copy(deep=True)
     counter_patience = 0
     epoch = 0
 
     while (counter_patience < patience) and (epoch < epochs):
-        n_features_before = X_new.shape[1]
-        print("=====================EPOCH", epoch+1, "=====================")
-        X_new = scan_features(X_new, y, model, verbose)
-        n_features_after = X_new.shape[1]
+        n_features_before = x_new.shape[1]
+        print("=====================EPOCH", epoch + 1, "=====================")
+        x_new = scan_features(x_new, labels, model, verbose)
+        n_features_after = x_new.shape[1]
 
         if n_features_before == n_features_after:
             counter_patience += 1
@@ -100,6 +100,6 @@ def get_relevant_features(
         epoch += 1
 
     if isinstance(filename_output, str):
-        X_new.to_csv(filename_output, index=False)
+        x_new.to_csv(filename_output, index=False)
 
-    return X_new
+    return x_new
